86f6838b63f0978c393e284746f35b6d0ae799d7
rishabhraj22 <rajrishabh2209@gmail.com>
2026-02-24 14:29:08 +0530

    Initial Maven project setup with JUnit

---
+++ src/test/java/com/apps/quantitymeasurement/QuantityTest.java
@@ -0,0 +1,11 @@
+package com.apps.quantitymeasurement;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class QuantityTest {
+
+    @Test
+    void sampleTest() {
+        assertTrue(true);
+    }
+}
